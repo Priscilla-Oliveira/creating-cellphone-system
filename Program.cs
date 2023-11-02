44a577c467871b807d7549f3653468df3842c401
Priscilla Oliveira <priscillac.oliveira@outlook.com>
2023-11-02 10:42:25 -0300

    final version 1.0

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo x", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("iPhone: ");
+Smartphone iphone = new Iphone(numero: "789101", modelo: "Modelo y", imei: "222222222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
